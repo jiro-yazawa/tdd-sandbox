9b54889e037fb9d8ab53b3be32baf578f456fb72
kj-yazawa <yazawa17ko@gmail.com>
2020-06-20 10:13:08 +0900

    create spec, finished with failure

--- spec/models/dollar_spec.rb
+++ spec/models/dollar_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Dollar, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '#multiple' do
+    five = Dollar.new(5)
+    five.times(2)
+    expect(five.amount).to eq 10
+  end
 end
