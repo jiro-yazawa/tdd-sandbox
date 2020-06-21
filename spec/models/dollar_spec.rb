b8b222ba8285cc6e8fc033ea58886bcec08b14a1
kj-yazawa <yazawa17ko@gmail.com>
2020-06-21 09:28:06 +0900

    テストの期待値の変更

--- spec/models/dollar_spec.rb
+++ spec/models/dollar_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Dollar, type: :model do
   it '#multiple' do
     five = Dollar.new(5)
-    five.times(2)
-    expect(five.amount).to eq 10
-    five.times(3)
-    expect(five.amount).to eq 15
+    product = five.times(2)
+    expect(product.amount).to eq 10
+    product = five.times(3)
+    expect(product.amount).to eq 15
   end
 end
