003f0469d8247b46919a32919a194b11a3413cb2
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-04 17:16:46 +0900

    equals?のスペックをmoney_specに移動

--- spec/models/dollar_spec.rb
+++ spec/models/dollar_spec.rb
@@ -6,9 +6,4 @@ RSpec.describe Dollar, type: :model do
     expect(Dollar.new(10).equal?(five.times(2))).to be true
     expect(Dollar.new(15).equal?(five.times(3))).to be true
   end
-
-  it '#equal?' do
-    expect(Dollar.new(5).equal?(Dollar.new(5))).to be true
-    expect(Dollar.new(5).equal?(Dollar.new(6))).to be false
-  end
 end
--- spec/models/franc_spec.rb
+++ spec/models/franc_spec.rb
@@ -6,9 +6,4 @@ RSpec.describe Franc, type: :model do
     expect(Franc.new(10).equal?(five.times(2))).to be true
     expect(Franc.new(15).equal?(five.times(3))).to be true
   end
-
-  it '#equal?' do
-    expect(Franc.new(5).equal?(Franc.new(5))).to be true
-    expect(Franc.new(5).equal?(Franc.new(6))).to be false
-  end
 end
