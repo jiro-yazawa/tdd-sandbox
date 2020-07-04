d8b8ee1c700da49f3b675622c26ca29400b70dd3
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-04 17:18:13 +0900

    FrancとDollarが等価であるかどうかのテストを追記

--- spec/models/money_spec.rb
+++ spec/models/money_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Money, type: :model do
     expect(Dollar.new(5).equal?(Dollar.new(6))).to be false
     expect(Franc.new(5).equal?(Franc.new(5))).to be true
     expect(Franc.new(5).equal?(Franc.new(6))).to be false
+    expect(Franc.new(5).equal?(Dollar.new(5))).to be false
   end
 end
