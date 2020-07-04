c50e10f543c9a957239357d21c433b26246c9264
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-04 13:39:51 +0900

    Moneyクラスの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_010725) do
+ActiveRecord::Schema.define(version: 2020_07_04_043852) do
 
   create_table "dollars", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_07_01_010725) do
   end
 
   create_table "francs", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "money", force: :cascade do |t|
+    t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
