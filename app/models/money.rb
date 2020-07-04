30d0ebaa551624c34269530a908d0959fad353b1
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-04 19:53:55 +0900

    初期化時にcurrencyを渡すよう変更

--- app/models/money.rb
+++ app/models/money.rb
@@ -1,8 +1,9 @@
 class Money < ApplicationRecord
   attr_reader :amount
 
-  def initialize(amount)
+  def initialize(amount, currency)
     @amount = amount
+    @currency = currency
   end
 
   def self.dollar(amount)
