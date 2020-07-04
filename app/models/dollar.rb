849b70e42c96756e2b5150a23925a8d5a09197ba
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-04 20:30:36 +0900

    テストを通すためにnewの引数にnilを渡す

--- app/models/dollar.rb
+++ app/models/dollar.rb
@@ -1,6 +1,13 @@
 class Dollar < Money
+  attr_reader :amount, :currency
+
+  def initialize(amount, currency)
+    @amount = amount
+    @currency = currency
+  end
+
   def times(multiplier)
-    return Dollar.new(amount * multiplier)
+    return Dollar.new(amount * multiplier, nil)
   end
 
   def currency
--- app/models/franc.rb
+++ app/models/franc.rb
@@ -1,6 +1,13 @@
 class Franc < Money
+  attr_reader :amount, :currency
+
+  def initialize(amount, currency)
+    @amount = amount
+    @currency = currency
+  end
+
   def times(multiplier)
-    return Franc.new(amount * multiplier)
+    return Franc.new(amount * multiplier, nil)
   end
 
   def currency
