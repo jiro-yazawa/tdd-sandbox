2756f1a23aba3d7ca3dec06732ed067be0eb0115
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-04 19:27:58 +0900

    サブクラスを消せる状態にするためにMoneyクラスにdollarメソッドを実装するようテストを変更

--- spec/models/dollar_spec.rb
+++ spec/models/dollar_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Dollar, type: :model do
   it '#multiple' do
-    five = Dollar.new(5)
+    five = Money.dollar(5)
     expect(Dollar.new(10).equal?(five.times(2))).to be true
     expect(Dollar.new(15).equal?(five.times(3))).to be true
   end
