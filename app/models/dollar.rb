bba18858fcb2296a1f8440926f0077709db46b87
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-04 19:50:56 +0900

    DollarクラスとFrancクラスにそれぞれcurrencyメソッドを実装した

--- app/models/dollar.rb
+++ app/models/dollar.rb
@@ -2,4 +2,8 @@ class Dollar < Money
   def times(multiplier)
     return Dollar.new(amount * multiplier)
   end
+
+  def currency
+    return 'USD'
+  end
 end
--- app/models/franc.rb
+++ app/models/franc.rb
@@ -2,4 +2,8 @@ class Franc < Money
   def times(multiplier)
     return Franc.new(amount * multiplier)
   end
+
+  def currency
+    return 'CHF'
+  end
 end
