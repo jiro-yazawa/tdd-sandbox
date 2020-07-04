c061aae1437e69b8d2b968f9c958ef76b0a9c844
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-04 20:34:56 +0900

    コンストラクタを親クラスに引き上げた

--- app/models/dollar.rb
+++ app/models/dollar.rb
@@ -1,11 +1,4 @@
 class Dollar < Money
-  attr_reader :amount, :currency
-
-  def initialize(amount, currency)
-    @amount = amount
-    @currency = currency
-  end
-
   def times(multiplier)
     return Money.dollar(amount * multiplier)
   end
--- app/models/franc.rb
+++ app/models/franc.rb
@@ -1,11 +1,4 @@
 class Franc < Money
-  attr_reader :amount, :currency
-
-  def initialize(amount, currency)
-    @amount = amount
-    @currency = currency
-  end
-
   def times(multiplier)
     return Money.franc(amount * multiplier)
   end
