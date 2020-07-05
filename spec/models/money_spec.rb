eb8d26ee49e8ac546673bfdccb3bdf0f6e159ce0
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-05 13:24:13 +0900

    2フランを1ドルに換算するためのテストを実装

--- spec/models/money_spec.rb
+++ spec/models/money_spec.rb
@@ -48,5 +48,12 @@ RSpec.describe Money, type: :model do
       result = bank.reduce(Money.dollar(1), 'USD')
       expect(Money.dollar(1).equals?(result))
     end
+
+    it 'reduce money different currency' do
+      bank = Bank.new
+      bank.add_rate('CHF', 'USD', 2)
+      result = bank.reduce(Money.franc(2), 'USD')
+      expect(Money.dollar(1).equals?(result)).to be_truthy
+    end
   end
 end
