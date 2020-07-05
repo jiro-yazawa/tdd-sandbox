4012166ef65969be02fe700b7c50aa7bb7a39b8c
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-05 14:03:07 +0900

    hashのキーを探索するためのメソッドをオーバーライド

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -9,4 +9,13 @@ class Pair
   def equals?(pair)
     from == pair.from && to == pair.to
   end
+
+  # NOTE: Hashのキー探索に使われるのが'eql?': https://docs.ruby-lang.org/ja/latest/method/Object/i/eql=3f.htmlhttps://docs.ruby-lang.org/ja/latest/method/Object/i/eql=3f.html
+  def eql?(other)
+    equals?(other)
+  end
+
+  def hash
+    0
+  end
 end
