a3af3edd03cd42dd91a9a4a7ab884cd6bbc2f79f
jiro-yazawa <yazawa17ko@gmail.com>
2020-07-05 14:03:39 +0900

    Bankのadd_rateメソッドの実装 + rateの検索メソッドの実装

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,10 +1,16 @@
 class Bank
-  def add_rate(from, to, rate)
+  attr_reader :rates
 
+  def initialize
+    @rates = {} # {Pair: integer}
+  end
+
+  def add_rate(from, to, rate)
+    rates[Pair.new(from, to)] = rate
   end
 
   def rate(from, to)
-    from == 'CHF' && to == 'USD' ? 2 : 1
+    rates[Pair.new(from, to)]
   end
 
   def reduce(sum, to)
